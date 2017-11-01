1570366ae4c38172b650d4d87fccb7d5cb6205c6
gongtong <gongtong@2dfire.com>
2017-11-01 21:38:41 +0800

    init

---
+++ BaseModule/src/main/java/middlem/person/basemodule/BaseActivity.java
@@ -0,0 +1,18 @@
+package middlem.person.basemodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/***********************************************
+ *
+ * <P> desc:   基类activty
+ * <P> Author: gongtong
+ * <P> Date: 2017-11-01 21:14
+ ***********************************************/
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ sample/src/main/java/middlem/person/baselib/SampleActivity.java
@@ -0,0 +1,11 @@
+package middlem.person.baselib;
+
+/***********************************************
+ * <P> desc:
+ * <P> Author: gongtong
+ * <P> Date: 2017/11/1 21:11
+ * <P> Copyright  2008 二维火科技
+ ***********************************************/
+
+public class SampleActivity  {
+}
